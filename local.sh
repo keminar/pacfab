762e5a6b26eca3c1b963b1a49d482b222ea957f8
minggui.li <651718290@qq.com>
2014-03-20 20:47:56 +0800

    增加LOCALHOST快捷脚本

---
+++ local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USER_SSH=/root/.ssh
+
+if [ ! -f $USER_SSH/id_rsa.pub ];then
+	ssh-keygen -t rsa -f $USER_SSH/id_rsa  -C '' -N '' -q
+fi
+
+if [ ! -f $USER_SSH/authorized_keys ];then
+	cat $USER_SSH/id_rsa.pub >> $USER_SSH/authorized_keys
+fi
+
+./setup.sh -H localhost -i $USER_SSH/id_rsa.pub "$@"
